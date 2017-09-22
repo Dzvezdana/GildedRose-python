528232d674ec139c466c612ca7bd36b612b7242f
Per Lundholm <per@artcomputer.se>
2017-09-22 12:29:41 +0200

    Added a relative day function. Understands today, tomorrow and yesterday. Added assert_equals as convenience.

--- features/steps/general.py
+++ features/steps/general.py
@@ -6,12 +6,22 @@ from item import Item
 use_step_matcher("re")
 
 
-@given("an item with quality (?P<quality>.+) and sell by date tomorrow")
-def step_impl(context, quality):
+def get_sell_in_relative_day(relativeDay):
+    if relativeDay == "yesterday":
+        return -1
+    if relativeDay == "today":
+        return 0
+    if relativeDay == "tomorrow":
+        return 1
+
+
+@given("an item with quality (?P<quality>.+) and sell by date (?P<relativeDay>.+)")
+def step_impl(context, quality, relativeDay):
     """
     :type context: behave.runner.Context
     """
-    context.item = Item("some name", 1, int(quality))
+    sell_in = get_sell_in_relative_day(relativeDay)
+    context.item = Item("some name", sell_in, int(quality))
     context.gildedRose = GildedRose()
     context.gildedRose.inventory.items.append(context.item)
     pass
@@ -30,8 +40,9 @@ def step_impl(context):
 def step_impl(context, quality):
     """
     :type context: behave.runner.Context
+    :type quality: str
     """
-    assert context.item.quality == int(quality)
+    assert_equals(context.item.quality, int(quality))
 
 
 @step("the item has sell by date today")
@@ -39,4 +50,9 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    assert context.item.sell_in == 0, "Sell in days expected 0, was: %r" % context.item.sell_in
+    assert_equals(context.item.sell_in, 0)
+
+
+def assert_equals(actual, expected):
+    assert actual == expected, "Expected {} {}, was: {} {}"\
+        .format(expected, type(expected), actual, type(actual))
